3ce61630117c1bdcc3e33ed0d71a890f70e479ec
AmmarWalyMajid <ammarwaly9a@gmail.com>
2021-07-10 10:48:03 +0700

    implemetasi Fragment

--- app/src/main/java/responsi/com/ui/MainActivity.java
+++ app/src/main/java/responsi/com/ui/MainActivity.java
@@ -1,16 +1,63 @@
 package responsi.com.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.view.MenuItem;
 
 import responsi.com.R;
+import responsi.com.fragment.HistoryFragment;
+import responsi.com.fragment.HomeFragment;
+import responsi.com.fragment.PayFragment;
+import responsi.com.fragment.SettingFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loadFragment(new HomeFragment());
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bn_main);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fl_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onNavigationItemSelected(@NonNull @org.jetbrains.annotations.NotNull MenuItem item) {
+
+        Fragment fragment = null;
+        switch (item.getItemId()){
+            case R.id.home_menu:
+                fragment = new HomeFragment();
+                break;
+            case R.id.pay_menu:
+                fragment = new PayFragment();
+                break;
+            case R.id.history_menu:
+                fragment = new HistoryFragment();
+                break;
+            case R.id.settings_menu:
+                fragment = new SettingFragment();
+                break;
+        }
+        return loadFragment(fragment);
     }
 }
