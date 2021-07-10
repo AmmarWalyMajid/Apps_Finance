f852838bfa008be3d8020b21d1da5c777074beab
AmmarWalyMajid <ammarwaly9a@gmail.com>
2021-07-10 09:25:42 +0700

    ui.fragment add library

---
+++ app/src/main/java/responsi/com/RecorveryActivity.java
@@ -0,0 +1,14 @@
+package responsi.com;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecorveryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recorvery);
+    }
+}
