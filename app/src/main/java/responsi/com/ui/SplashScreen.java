79739e50846dc3b869c495aed44015fca7e87c49
AmmarWalyMajid <ammarwaly9a@gmail.com>
2021-07-10 11:34:43 +0700

    SplashScreen

--- app/src/main/java/responsi/com/ui/SplashScreen.java
+++ app/src/main/java/responsi/com/ui/SplashScreen.java
@@ -2,15 +2,30 @@ package responsi.com.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import responsi.com.R;
 
 public class SplashScreen extends AppCompatActivity {
 
+    private int load=3000;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(() -> {
+            //if(
+            // )
+            Intent home=new Intent(SplashScreen.this, LoginActivity.class);
+            startActivity(home);
+            finish();
+
+        },load);
     }
 }
