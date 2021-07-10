e366c27c1c46e0d072d55c19d31a1b174b9c987b
AmmarWalyMajid <ammarwaly9a@gmail.com>
2021-07-10 11:44:22 +0700

    RecorveryActivity

--- app/src/main/java/responsi/com/ui/RecorveryActivity.java
+++ app/src/main/java/responsi/com/ui/RecorveryActivity.java
@@ -2,15 +2,23 @@ package responsi.com.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import responsi.com.R;
 
 public class RecorveryActivity extends AppCompatActivity {
 
+    private ImageView back;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recorvery);
+
+        back = findViewById(R.id.backRec);
+        back.setOnClickListener(v -> startActivity(new Intent(RecorveryActivity.this, LoginActivity.class)));
+
     }
 }
